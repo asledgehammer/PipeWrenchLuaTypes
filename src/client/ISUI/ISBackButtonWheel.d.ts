51a67037f92a23626f1ce41d44c10a3c6f0d3049
JabDoesThings <JabDoesThings@github.com>
2022-05-16 13:31:34 -0500

    Typed `src/client/ISUI/ISBackButtonWheel.d.ts`.

---
+++ src/client/ISUI/ISBackButtonWheel.d.ts
@@ -0,0 +1,21 @@
+declare module 'ISUI' {
+    import { zombie } from 'Zomboid';
+
+    export class ISBackButtonWheelInstance extends ISRadialMenuInstance {
+        protected constructor();
+
+        __index: ISBackButtonWheelInstance;
+
+        center(): void;
+        addCommands(): void;
+        onGainJoypadFocus(joypadData: any): void;
+        onLoseJoypadFocus(joypadData: any): void;
+        onJoypadDown(button: ISButtonInstance, joypadData: any): void;
+        onCommand(command: string): void;
+    }
+
+    export class ISBackButtonWheel {
+        private constructor();
+        static new(playerNum: number): ISBackButtonWheelInstance;
+    }
+}
