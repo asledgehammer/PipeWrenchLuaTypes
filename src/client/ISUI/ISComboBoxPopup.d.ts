ba4b57d35ffe1ff65bb44df4943a9a9c851918b8
JabDoesThings <JabDoesThings@github.com>
2022-05-16 18:30:29 -0500

    #1 Typed `src/client/ISUI/ISComboBoxPopup.d.ts`.

--- src/client/ISUI/ISComboBoxPopup.d.ts
+++ src/client/ISUI/ISComboBoxPopup.d.ts
@@ -1,17 +1,17 @@
 import { zombie } from "Zomboid";
-
-// TODO: Implement.
+type InventoryItem = zombie.inventory.InventoryItem;
 
 declare module 'ISUI' {
     export class ISComboBoxPopupInstance {
         protected constructor();
 
-
+        doDrawItem(y: number, item: InventoryItem, alt?: any /* Unused */): number;
+        setComboBox(comboBox: ISComboBoxInstance): void;
     }
 
     export class ISComboBoxPopup {
         private constructor();
 
-        static new(): ISComboBoxPopupInstance;
+        static new(x: number, y: number, width: number, height: number): ISComboBoxPopupInstance;
     }
 }
