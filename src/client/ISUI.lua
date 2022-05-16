c129c9e0c969e5fc5fd3317b6f401036d3fa46e3
JabDoesThings <JabDoesThings@github.com>
2022-05-16 13:29:21 -0500

    Typed `src/client/ISUI.lua` directory.

---
+++ src/client/ISUI.lua
@@ -0,0 +1,19 @@
+local Exports = {}
+Exports.FPS = loadstring('return FPS')();
+Exports.InterpolationPeriodDebug = loadstring('return InterpolationPeriodDebug')();
+Exports.InterpolationPlayerPeriodDebug = loadstring('return InterpolationPlayerPeriodDebug')();
+Exports.ISAlarmClockDialog = loadstring('return ISAlarmClockDialog')();
+Exports.ISBackButtonWheel = loadstring('return ISBackButtonWheel')();
+
+Exports.ISButton = loadstring('return ISButton')();
+Exports.ISCollapsableWindow = loadstring('return ISCollapsableWindow')();
+Exports.ISLabel = loadstring('return ISLabel')();
+Exports.ISPanel = loadstring('return ISPanel')();
+Exports.ISPanelJoypad = loadstring('return ISPanelJoypad')();
+Exports.ISRadialMenu = loadstring('return ISRadialMenu')();
+
+Exports.ISRect = loadstring('return ISRect')();
+Exports.ISResizeWidget = loadstring('return ISResizeWidget')();
+Exports.ISScrollBar = loadstring('return ISScrollBar')();
+Exports.ISUIElement = loadstring('return ISUIElement')();
+return Exports
