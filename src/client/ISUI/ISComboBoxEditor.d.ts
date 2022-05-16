721d4676ddd2e83e54d1ed59da61fce6304d0837
JabDoesThings <JabDoesThings@github.com>
2022-05-16 18:27:03 -0500

    #1 Typed `src/client/ISUI/ISComboBoxEditor.d.ts`.

--- src/client/ISUI/ISComboBoxEditor.d.ts
+++ src/client/ISUI/ISComboBoxEditor.d.ts
@@ -1,15 +1,24 @@
-import { zombie } from "Zomboid";
+import { zombie } from 'Zomboid';
 
 // TODO: Implement.
 
 declare module 'ISUI' {
     export class ISComboBoxEditorInstance extends ISTextEntryBoxInstance {
         protected constructor();
+
+        comboBox: ISComboBoxInstance;
+        parentCombo: ISComboBoxInstance;
     }
 
     export class ISComboBoxEditor {
         private constructor();
 
-        static new(): ISComboBoxEditorInstance;
+        static new(
+            x: number,
+            y: number,
+            width: number,
+            height: number,
+            comboBox: ISComboBoxInstance
+        ): ISComboBoxEditorInstance;
     }
 }
