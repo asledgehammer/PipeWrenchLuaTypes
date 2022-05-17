c26e7059563b495aae43af362e4c93ebfb8c6bd6
JabDoesThings <JabDoesThings@github.com>
2022-05-17 13:13:50 -0500

    #1 Cleaned `ISPanel`.

--- src/client/ISUI/ISPanel.d.ts
+++ src/client/ISUI/ISPanel.d.ts
@@ -2,15 +2,14 @@ declare module 'ISUI' {
     export class ISPanelInstance extends ISUIElementInstance {
         protected constructor();
 
+        borderColor: RGBA;
+        backgroundColor: RGBA;
         background: boolean;
-        backgroundColor: { r: number; g: number; b: number; a: number };
-        borderColor: { r: number; g: number; b: number; a: number };
         moveWithMouse: boolean;
-
-        downX: number | null;
-        downY: number | null;
-        moving: boolean | null;
-        mouseOver: boolean | null;
+        downX: number;
+        downY: number;
+        moving: boolean;
+        mouseOver: boolean;
 
         noBackground(): void;
         close(): void;
@@ -18,15 +17,7 @@ declare module 'ISUI' {
 
     export class ISPanel {
         private constructor();
-        
-        /**
-         * @param x (double)
-         * @param y (double)
-         * @param width (double)
-         * @param height (double)
-         *
-         * @returns The new ISPanel instance.
-         */
+
         static new(x: number, y: number, width: number, height: number): ISPanelInstance;
     }
 }
