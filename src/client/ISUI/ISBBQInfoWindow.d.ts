35d312e196bea467c6de7f9a2d9ea78afcf9c531
JabDoesThings <JabDoesThings@github.com>
2022-05-17 12:13:18 -0500

    #1 Cleaned `ISBBQInfoWIndow`.

--- src/client/ISUI/ISBBQInfoWindow.d.ts
+++ src/client/ISUI/ISBBQInfoWindow.d.ts
@@ -1,23 +1,25 @@
-declare module 'ISUI' {
-    import { zombie } from "Zomboid";
+import { zombie } from 'Zomboid';
+type IsoPlayer = zombie.characters.IsoPlayer;
+type IsoBarbecue = zombie.iso.objects.IsoBarbecue;
 
+declare module 'ISUI' {
     export class ISBBQInfoWindowInstance extends ISCollapsableWindowInstance {
         protected constructor();
-        
-        object: zombie.iso.objects.IsoBarbecue;
-        character: zombie.characters.IsoPlayer;
+
+        object: IsoBarbecue;
+        character: IsoPlayer;
         playerNum: number;
         panel: ISToolTipInstance;
         spriteName: string;
         isLit: boolean;
         fuelAmount: number;
 
-        setObject(bbq: zombie.iso.objects.IsoBarbecue): void;
+        setObject(bbq: IsoBarbecue): void;
     }
 
     export class ISBBQInfoWindow {
         private constructor();
 
-        static new(x: number, y: number, character: zombie.characters.IsoPlayer, bbq: zombie.iso.objects.IsoBarbecue): ISBBQInfoWindowInstance;
+        static new(x: number, y: number, character: IsoPlayer, bbq: IsoBarbecue): ISBBQInfoWindowInstance;
     }
 }
