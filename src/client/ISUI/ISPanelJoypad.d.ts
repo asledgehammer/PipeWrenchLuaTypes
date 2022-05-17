77234ff41cf71dfaa3c2826ee473f3f8e834a5db
JabDoesThings <JabDoesThings@github.com>
2022-05-17 13:19:49 -0500

    #1 Cleaned `ISPanelJoypad`.

--- src/client/ISUI/ISPanelJoypad.d.ts
+++ src/client/ISUI/ISPanelJoypad.d.ts
@@ -1,52 +1,37 @@
 declare module 'ISUI' {
-    import { zombie } from 'Zomboid';
-
-    export type ISJoypadData = {};
-
     export class ISPanelJoypadInstance extends ISUIElementInstance {
         protected constructor();
 
-        __index: ISUIElementInstance;
-        x: number;
-        y: number;
-        background: boolean;
-        backgroundColor: ISUIColor;
-        borderColor: ISUIColor;
-        width: number;
-        height: number;
-        anchorLeft: boolean;
-        anchorRight: boolean;
-        anchorTop: boolean;
-        anchorBottom: boolean;
         joypadButtons: { [offset: number]: ISButtonInstance };
-        joypadIndex: number;
         joypadButtonsY: { [offset: number]: ISButtonInstance };
+        ISButtonA: ISButtonInstance;
+        ISButtonB: ISButtonInstance;
+        ISButtonX: ISButtonInstance;
+        ISButtonY: ISButtonInstance;
+        backgroundColor: RGBA;
+        borderColor: RGBA;
+        joypadIndex: number;
         joypadIndexY: number;
+        downX: number;
+        downY: number;
+        moving: boolean;
+        mouseOver: boolean;
         moveWithMouse: boolean;
-
-        ISButtonA: ISButtonInstance | null;
-        ISButtonB: ISButtonInstance | null;
-        ISButtonX: ISButtonInstance | null;
-        ISButtonY: ISButtonInstance | null;
-
-        moving: boolean | null;
-        downX: number | null;
-        downY: number | null;
-        mouseOver: boolean | null;
+        background: boolean;
 
         initialise(): void;
-        setVisible(visible: boolean, joypadData: ISJoypadData): void;
+        setVisible(visible: boolean, joypadData: JoypadData): void;
         insertNewLineOfButtons(
-            button1: ISButtonInstance | null,
-            button2: ISButtonInstance | null,
-            button3: ISButtonInstance | null,
-            button4: ISButtonInstance | null,
-            button5: ISButtonInstance | null,
-            button6: ISButtonInstance | null,
-            button7: ISButtonInstance | null,
-            button8: ISButtonInstance | null,
-            button9: ISButtonInstance | null,
-            button10: ISButtonInstance | null
+            button1: ISButtonInstance,
+            button2: ISButtonInstance,
+            button3: ISButtonInstance,
+            button4: ISButtonInstance,
+            button5: ISButtonInstance,
+            button6: ISButtonInstance,
+            button7: ISButtonInstance,
+            button8: ISButtonInstance,
+            button9: ISButtonInstance,
+            button10: ISButtonInstance
         ): void;
 
         insertNewListOfButtons(list: { [offset: number]: ISButtonInstance }): void;
@@ -57,17 +42,17 @@ declare module 'ISUI' {
         setISButtonForB(button: ISButtonInstance): void;
         setISButtonForY(button: ISButtonInstance): void;
         setISButtonForX(button: ISButtonInstance): void;
-        onJoypadDown(button: ISButtonInstance, joypadData: ISJoypadData): void;
+        onJoypadDown(button: ISButtonInstance, joypadData: JoypadData): void;
         getVisibleChildren(joypadIndexY: number): { [offset: number]: ISUIElementInstance };
         getClosestChild(children: { [offset: number]: ISUIElementInstance }, x: number): number;
-        onJoypadDirLeft(joypadData: ISJoypadData): void;
-        onJoypadDirRight(joypadData: ISJoypadData): void;
-        onJoypadDirUp(joypadData: ISJoypadData): void;
-        onJoypadDirDown(joypadData: ISJoypadData): void;
-        getJoypadFocus(): ISUIElementInstance | null;
-        setJoypadFocus(child: ISUIElementInstance, joypadData: ISJoypadData): void;
-        restoreJoypadFocus(joypadData: ISJoypadData): void;
-        clearJoypadFocus(joypadData: ISJoypadData): void;
+        onJoypadDirLeft(joypadData: JoypadData): void;
+        onJoypadDirRight(joypadData: JoypadData): void;
+        onJoypadDirUp(joypadData: JoypadData): void;
+        onJoypadDirDown(joypadData: JoypadData): void;
+        getJoypadFocus(): ISUIElementInstance;
+        setJoypadFocus(child: ISUIElementInstance, joypadData: JoypadData): void;
+        restoreJoypadFocus(joypadData: JoypadData): void;
+        clearJoypadFocus(joypadData: JoypadData): void;
         ensureVisible(): void;
         isFocusOnControl(): boolean;
         onMouseUp(x: number, y: number): void;
