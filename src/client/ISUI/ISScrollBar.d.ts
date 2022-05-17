69f8b753e120f0b4834e2f6bce4735622f2842d4
JabDoesThings <JabDoesThings@github.com>
2022-05-17 13:33:10 -0500

    #1 Cleaned `ISScrollBar`.

--- src/client/ISUI/ISScrollBar.d.ts
+++ src/client/ISUI/ISScrollBar.d.ts
@@ -1,31 +1,30 @@
-declare module 'ISUI' {
-    import { zombie } from 'Zomboid';
+import { zombie } from 'Zomboid';
+type Texture = zombie.core.textures.Texture;
 
+declare module 'ISUI' {
     export class ISScrollBarInstance extends ISUIElementInstance {
         protected constructor();
 
-        backgroundColor: { r: number; g: number; b: number; a: number };
-        borderColor: { r: number; g: number; b: number; a: number };
-        vertical: boolean;
+        uptex: Texture;
+        downtex: Texture;
+        toptex: Texture;
+        midtex: Texture;
+        bottex: Texture;
+        backgroundColor: RGBA;
+        borderColor: RGBA;
+        barx: number;
+        bary: number;
+        barwidth: number;
+        barheight: number;
         pos: number;
+        vertical: boolean;
         scrolling: boolean;
         background: boolean;
-
-        uptex: zombie.core.textures.Texture;
-        downtex: zombie.core.textures.Texture;
-        toptex: zombie.core.textures.Texture;
-        midtex: zombie.core.textures.Texture;
-        bottex: zombie.core.textures.Texture;
-
-        barx: number | null;
-        bary: number | null;
-        barwidth: number | null;
-        barheight: number | null;
-        doSetStencil: boolean | null;
+        doSetStencil: boolean;
         /** @deprecated */
-        doRepaintStencil: boolean | null;
+        doRepaintStencil: boolean;
 
-        onMouseDown(x: number, y: number): boolean | null;
+        onMouseDown(x: number, y: number): boolean;
         refresh(): void;
         updatePos(): void;
     }
@@ -33,12 +32,6 @@ declare module 'ISUI' {
     export class ISScrollBar {
         private constructor();
 
-        /**
-         * @param parent (ISUIElement)
-         * @param vertical (boolean)
-         *
-         * @returns The new ISScrollBar instance.
-         */
         static new(parent: ISUIElementInstance, vertical: boolean): ISScrollBarInstance;
     }
 }
