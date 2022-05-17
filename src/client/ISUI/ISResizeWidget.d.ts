c597f8795a1d9744a2946fb8438b28c9d5be3591
JabDoesThings <JabDoesThings@github.com>
2022-05-17 13:30:52 -0500

    #1 Typed `src/client/ISUI/ISResizeWidget.d.ts`.

--- src/client/ISUI/ISResizeWidget.d.ts
+++ src/client/ISUI/ISResizeWidget.d.ts
@@ -1,9 +1,26 @@
 declare module 'ISUI' {
     export class ISResizeWidgetInstance extends ISPanelInstance {
         protected constructor();
+
+        backgroundColorMouseOver: RGBA;
+        resizeTarget: any;
+        yonly: boolean;
+        resizing: boolean;
+
+        resize(deltaX: number, deltaY: number): void;
+        onMouseDown(x: number, y: number): boolean | void;
     }
 
     export class ISResizeWidget {
-        static new(): ISResizeWidgetInstance;
+        private constructor();
+
+        static new(
+            x: number,
+            y: number,
+            width: number,
+            height: number,
+            resizeTarget: any,
+            yonly: boolean
+        ): ISResizeWidgetInstance;
     }
 }
