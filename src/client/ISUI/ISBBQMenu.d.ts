81b69747e2806074d28a0a19b4e1243c2f97c953
JabDoesThings <JabDoesThings@github.com>
2022-05-17 12:01:46 -0500

    #1 Cleaned up `ISBBQMenu`.

--- src/client/ISUI/ISBBQMenu.d.ts
+++ src/client/ISUI/ISBBQMenu.d.ts
@@ -1,21 +1,57 @@
-declare module 'ISUI' {
-    import { zombie } from "Zomboid";
+import { zombie } from 'Zomboid';
+type IsoPlayer = zombie.characters.IsoPlayer;
+type InventoryItem = zombie.inventory.InventoryItem;
+type IsoObject = zombie.iso.IsoObject;
+type IsoBarbecue = zombie.iso.objects.IsoBarbecue;
 
+declare module 'ISUI' {
     export class ISBBQWindow {
         private constructor();
 
-        static OnFillWorldObjectContextMenu(player: zombie.characters.IsoPlayer, context: ISContextMenuInstance, worldobjects: zombie.iso.IsoObject[], test: boolean): boolean;
-        static onDisplayInfo(worldobjects: zombie.iso.IsoObject[], player: zombie.characters.IsoPlayer, bbq: zombie.iso.objects.IsoBarbecue): void;
-        static FindPropaneTank(player: zombie.characters.IsoPlayer, bbq: zombie.iso.objects.IsoBarbecue): zombie.inventory.InventoryItem;
-        static onAddFuel(playerObj: zombie.characters.IsoPlayer, bbq: zombie.iso.objects.IsoBarbecue, fuelType: string): void;
-        static onAddMultipleFuel(playerObj: zombie.characters.IsoPlayer, bbq: zombie.iso.objects.IsoBarbecue): void;
-        static onAddAllFuel(playerObj: zombie.characters.IsoPlayer, bbq: zombie.iso.objects.IsoBarbecue): void;
-        static onLightFromLiterature(playerObj: zombie.characters.IsoPlayer, itemType: string, lighter: zombie.inventory.InventoryItem, bbq: zombie.iso.objects.IsoBarbecue): void;
-        static onLightFromPetrol(worldobjects: zombie.iso.IsoObject[], player: zombie.characters.IsoPlayer, lighter: zombie.inventory.InventoryItem, petrol: zombie.inventory.InventoryItem, bbq: zombie.iso.objects.IsoBarbecue): void;
-        static onLightFromKindle(worldobjects: zombie.iso.IsoObject[], player: zombie.characters.IsoPlayer, percedWood: zombie.inventory.InventoryItem, stickOrBranch: zombie.inventory.InventoryItem, bbq: zombie.iso.objects.IsoBarbecue): void;
-        static onExtinguish(worldobjects: zombie.iso.IsoObject[], player: zombie.characters.IsoPlayer, bbq: zombie.iso.objects.IsoBarbecue): void;
-        static onInsertPropaneTank(worldobjects: zombie.iso.IsoObject[], player: zombie.characters.IsoPlayer, bbq: zombie.iso.objects.IsoBarbecue, tank: zombie.inventory.InventoryItem): void;
-        static onRemovePropaneTank(worldobjects: zombie.iso.IsoObject[], player: zombie.characters.IsoPlayer, bbq: zombie.iso.objects.IsoBarbecue, tank: zombie.inventory.InventoryItem): void;
-        static onToggle(worldobjects: zombie.iso.IsoObject[], player: zombie.characters.IsoPlayer, bbq: zombie.iso.objects.IsoBarbecue, tank: zombie.inventory.InventoryItem): void;
+        static OnFillWorldObjectContextMenu(
+            player: IsoPlayer,
+            context: ISContextMenuInstance,
+            worldobjects: IsoObject[],
+            test: boolean
+        ): boolean;
+        static onDisplayInfo(worldobjects: IsoObject[], player: IsoPlayer, bbq: IsoBarbecue): void;
+        static FindPropaneTank(player: IsoPlayer, bbq: IsoBarbecue): InventoryItem;
+        static onAddFuel(playerObj: IsoPlayer, bbq: IsoBarbecue, fuelType: string): void;
+        static onAddMultipleFuel(playerObj: IsoPlayer, bbq: IsoBarbecue): void;
+        static onAddAllFuel(playerObj: IsoPlayer, bbq: IsoBarbecue): void;
+        static onLightFromLiterature(
+            playerObj: IsoPlayer,
+            itemType: string,
+            lighter: InventoryItem,
+            bbq: IsoBarbecue
+        ): void;
+        static onLightFromPetrol(
+            worldobjects: IsoObject[],
+            player: IsoPlayer,
+            lighter: InventoryItem,
+            petrol: InventoryItem,
+            bbq: IsoBarbecue
+        ): void;
+        static onLightFromKindle(
+            worldobjects: IsoObject[],
+            player: IsoPlayer,
+            percedWood: InventoryItem,
+            stickOrBranch: InventoryItem,
+            bbq: IsoBarbecue
+        ): void;
+        static onExtinguish(worldobjects: IsoObject[], player: IsoPlayer, bbq: IsoBarbecue): void;
+        static onInsertPropaneTank(
+            worldobjects: IsoObject[],
+            player: IsoPlayer,
+            bbq: IsoBarbecue,
+            tank: InventoryItem
+        ): void;
+        static onRemovePropaneTank(
+            worldobjects: IsoObject[],
+            player: IsoPlayer,
+            bbq: IsoBarbecue,
+            tank: InventoryItem
+        ): void;
+        static onToggle(worldobjects: IsoObject[], player: IsoPlayer, bbq: IsoBarbecue, tank: InventoryItem): void;
     }
 }
